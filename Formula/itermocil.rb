4034b5c5feda2afddcf91b127bb39697b4d956d9
Maher Hawash <gmhawash@gmail.com>
2019-07-08 06:19:54 -0700

    Update home-brew formula

--- Formula/itermocil.rb
+++ Formula/itermocil.rb
@@ -5,8 +5,8 @@
 class Itermocil < Formula
   desc "Create pre-defined window/pane layouts and run commands in iTerm - alternative profile support implementation"
   homepage "https://github.com/gmhawash/itermocil"
-  url "https://github.com/gmhawash/itermocil/archive/0.2.2.zip"
-  sha256 "68a09466d2f59f4ed041295d5612d173e632d12a8f414ab7936195268fad5536"
+  url "https://github.com/gmhawash/itermocil/archive/0.2.3.zip"
+  sha256 "dae5aa876b7c5c56988307dfa4571c0c2363c1a31d78029b84e7ef0e27cad897"
 
 
   # depends_on "cmake" => :build
@@ -14,7 +14,7 @@ class Itermocil < Formula
     url "https://pypi.python.org/packages/source/P/PyYAML/PyYAML-3.11.tar.gz"
     sha256 "c36c938a872e5ff494938b33b14aaa156cb439ec67548fcab3535bb78b0846e8"
   end
-  
+
   def install
     ENV.prepend_create_path "PYTHONPATH", libexec/"vendor/lib/python2.7/site-packages"
 
